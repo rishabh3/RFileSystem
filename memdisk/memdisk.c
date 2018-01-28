6991f83c7e339ae168786b07f95b885e4a3a39bd
Rishabh Dutt <rishabh97dutt@gmail.com>
2018-01-28 20:51:31 +0530

    Memory disk is completed

--- memdisk/memdisk.c
+++ memdisk/memdisk.c
@@ -14,10 +14,15 @@
 struct memdisk memdisk[];
 
 static int num_blocks = 0; // This will increase when the block is completely full
-static int nreads = 0; // This will increase when the inode block is filled and then reset when one block is completely filled
-static int nwrites = 0;
+static int nreads = 0; // This will keep track of number of reads.
+static int nwrites = 0; // This will keep track of number of writes to disk.
 
 void sanity_check(int block_index, union block * data, int type_of_operation){
+
+	/*																*
+	 * Sanity Check functionality for error checking for parameters.*
+	 * 																*/
+
 	if(block_index < 0){
 		printf("ERROR: block number (%d) is less than 0", block_index);
 		abort();
@@ -33,19 +38,41 @@ void sanity_check(int block_index, union block * data, int type_of_operation){
 }
 
 int disk_size(){
+
+	/* Return Remaining disk size*/
+
 	return num_blocks;
 }
 
 void disk_read(int block_num, char * data){
+
+	/* Disk Read fuctionality */
+
 	sanity_check(block_num, data, READ);
-	memcpy(data, memdisk[block_num].disk_data, strlen(memdisk[block_num].disk_data));
-	nreads++;
+	if(memdisk[block_num].in_use == IN_USE){
+		printf("ERROR: Reading the disk at block (%d).\n", block_num);
+		abort();
+	}
+	else{
+		memcpy(data, memdisk[block_num].disk_data, strlen(memdisk[block_num].disk_data));
+		nreads++;
+	}
 }
 
 void disk_write(int block_num, char* data){
+
+	/* Disk Write Functionality */
+
 	sanity_check(block_num, data, WRITE);
-	memcpy(memdisk[block_num].disk_data, data, strlen(data));
-	nwrites++;
+	if(memdisk[block_num].in_use == IN_USE){
+		printf("ERROR: Writing to the disk at block (%d).\n", block_num);
+		abort();
+	}
+	else{
+		memcpy(memdisk[block_num].disk_data, data, strlen(data));
+		memdisk[block_num].in_use = IN_USE;
+		nwrites++;
+	}
 }
 
 
--- memdisk/memdisk.h
+++ memdisk/memdisk.h
@@ -8,12 +8,11 @@
 #ifndef MEMDISK_MEMDISK_H_
 #define MEMDISK_MEMDISK_H_
 
-#define DISK_BLK_SIZE 4096
-#define NUM_BLOCKS 125000
-#define IN_USE 0x01
-#define FULL 0x02
-#define READ 1
-#define WRITE 2
+#define DISK_BLK_SIZE 4096 // Disk Block Size
+#define NUM_BLOCKS 125000 // Total Disk Size around 500 MB
+#define IN_USE 0x01	// Flags for checking in_use or not
+#define READ 1 // Flags for read
+#define WRITE 2 // Flags for write
 
 struct memdisk;
 
@@ -21,11 +20,11 @@ extern struct memdisk{
 	char disk_data[DISK_BLK_SIZE - sizeof(int)];
 	int in_use;
 }memdisk[NUM_BLOCKS];
+// This is the in memory disk for storage emulator.
 
-
-int disk_size();
-void disk_read(int ,union rfs_block *);
-void disk_write(int ,union rfs_block *, int);
+int disk_size(); // Return the remaining size of the disk.
+void disk_read(int ,union rfs_block *); // Read from the disk.
+void disk_write(int ,union rfs_block *, int); // Write to the disk.
 
 
 #endif /* MEMDISK_MEMDISK_H_ */
