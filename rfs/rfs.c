b2edfdf2a218b5579ab910e10834d85ff040cf4a
Rishabh Dutt <rishabh97dutt@gmail.com>
2018-01-29 16:55:43 +0530

    Rfs some more functionalities

--- rfs/rfs.c
+++ rfs/rfs.c
@@ -7,6 +7,7 @@
 
 #include <string.h>
 #include <rfs.h>
+#include <../memdisk/memdisk.h>
 
 // Utility function to convert union to string.
 void convert_union_to_string(union rfs_block *block_data, char* data){
@@ -18,9 +19,27 @@ void convert_string_to_union(char* data, union rfs_block *block_data){
 	memcpy(block_data, data, sizeof(union rfs_block));
 }
 
+void print_info(union rfs_block block, int* num_inodes){
+	if(num_inodes > )
+}
+
 /* scan a mounted fs and prints info on superblock and each inode */
 void rfs_debug(){
-
+	union rfs_block s_block, temp;
+	char s_block_data_frm_disk[BLK_SIZE];
+	disk_read(SUPER_BLOCK, s_block_data_frm_disk);
+	convert_string_to_union(s_block_data_frm_disk, &s_block);
+	printf("SuperBlock: \n");
+	printf("	%d blocks\n", s_block.super.num_blocks);
+	printf(" 	%d inode blocks\n", s_block.super.num_inodeblocks);
+	printf(" 	%d inodes \n", s_block.super.num_inodes);
+	memset(s_block_data_frm_disk, '', BLK_SIZE);
+	int first_inode_block = 1;
+	for(int i = first_inode_block;i <= s_block.super.num_inodeblocks;i++){
+		disk_read(i, s_block_data_frm_disk);
+		convert_string_to_union(s_block_data_frm_disk, &temp);
+		print_info(temp, &s_block.super.num_inodes);
+	}
 }
 
 //writes a new file system onto the disk,re writes super block
